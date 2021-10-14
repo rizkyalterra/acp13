b5fc0bdc77f03a5325060fa59199ebe225607799
Rizky <rizky.kurniawan@alterra.id>
2021-10-14 20:00:01 +0700

    init

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(fibo(50)) // fibo(3) + fibo(2)
+}
+
+var i = 0
+
+func fibo(n int) int {
+	if n <= 1 {
+		return n
+	}
+	return fibo(n-1) + fibo(n-2)
+}
